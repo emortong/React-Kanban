72bfb113f2d724e72882409800ccdf432f6791b8
Estefania Morton <estefania.morton@gmail.com>
2017-02-08 16:59:54 -1000

    updating master

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,55 +6,54 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   devtool: 'eval-source-map',
   entry: [
-    'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, './app/entry.js'),
+    path.join(__dirname, '/app/entry.js'),
   ],
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
     publicPath: '/',
   },
-  resolve: {
-    extensions: ['', '.js'],
-  },
+  resolve: {},
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.tpl.html',
       inject: 'body',
       filename: 'index.html',
     }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      '__NODE_ENV__': JSON.stringify(process.env.NODE_ENV),
     }),
   ],
   module: {
-    loaders: [{
+    rules: [{
       test: /(\.js$|\.jsx$)/,
       exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015'],
-      },
-    }, {
-      test: /\.json?$/,
-      loader: 'json',
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'react',
+              'es2015'
+            ],
+          },
+        }
+      ]
     }, {
       test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
-      loader: 'file',
+      use: 'file-loader',
     }, {
       test: /\.(mp4|webm)$/,
-      loader: 'url?limit=10000'
+      use: 'url?limit=10000'
     }, {
       test: /(\.scss$|\.css$)/,
-      loaders: [
-        'style',
-        'css?modules&importLoaders=1' +
-        '&localIdentName=[path][local]__[hash:base64:5]!sass',
-        'sass',
-      ],
+      use: [
+        "style-loader",
+        "css-loader?modules&importLoaders=1" + "&localIdentName=[path][local]__[hash:base64:5]",
+        "sass-loader"
+      ]
     }],
   }
-}
+};
