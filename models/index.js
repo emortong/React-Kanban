73bb45874a75ca705879f283118165857e4b236c
Estefania Morton <estefania.morton@gmail.com>
2017-02-08 18:50:13 -1000

    host

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var PORT      = process.env.PORT || 5432;
-var HOST      = 'stark-shore-14804';
+var HOST      = 'stark-shore-14804.herokuapp.com';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
